f4579de1b63148ac3bf8d6b3485383f7fdb860ee
AttractAcquisition <attractacquisition@gmail.com>
2026-05-03 08:18:53 +0000

    Change port to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.static(join(__dirname, 'dist')))
